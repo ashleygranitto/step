c24d0cc26cd97f907e37ac284553d8b475e4ee63
Ashley Granitto <granittoashley@gmail.com>
2020-06-01 16:51:03 -0400

    add json parsing and formatting

--- portfolio/src/main/webapp/load-index.js
+++ portfolio/src/main/webapp/load-index.js
@@ -1,6 +1,18 @@
 // Set the nametag's HTML to be that fetched from the server
 async function getNametagText() {
   const response = await fetch('/nametag-text');
-  const greeting = await response.text();
-  document.getElementById('nametag').innerHTML = greeting;
+  const greetings = await response.json();
+
+  // Properly format and place the fetched greetings within the nametag
+  const nametag = document.getElementById('nametag'); 
+  nametag.innerHTML = formatComments(greetings);
+}
+
+// Format a list of comments such that their is a newline between each
+function formatComments (comments) {
+  master = '';
+  comments.forEach((comment) => {
+    master += comment + '</br>'
+  });
+  return master; 
 }
