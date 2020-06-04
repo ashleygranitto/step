6df62f0e910527bdbe54a634ce3ff59c183ee35f
Ashley Granitto <granittoashley@gmail.com>
2020-06-04 14:49:27 +0000

    use more meaningful variable name, reduce redundancy

--- portfolio/src/main/webapp/comment-form.js
+++ portfolio/src/main/webapp/comment-form.js
@@ -1,17 +1,19 @@
+const DEFAULT_COMMENT_COUNT = 3;
+
 /** Retrieve and post the default quantity of user comments */
-async function getComments() {
-  postFeed(3);
+function getComments() {
+  postFeed(DEFAULT_COMMENT_COUNT);
 }
 
 /** Update the feed to display the quantity of comments the user requested */
-async function updateComments(button) {
-  postFeed(button.value);
+function updateComments() {
+  postFeed(event.target.value);
 }
 
 /** Properly fetch, format, and place the specified quantity of comments within the feed */
 async function postFeed(quantity) {
   // Retrieve desired amount of comments
-  const response = await fetch('/handle-comment?value=' + quantity);
+  const response = await fetch('/handle-comment?quantity=' + quantity);
   const comments = await response.json();
   
   // Post comments to the feed
