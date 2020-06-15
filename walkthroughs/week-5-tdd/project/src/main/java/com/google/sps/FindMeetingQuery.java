1a3f4ca154398731e805772d68fb8e611e484ee4
Ashley Granitto <granittoashley@gmail.com>
2020-06-15 15:38:56 +0000

    utilize stream, immutable lists, elucidate some bulky code

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,90 +14,92 @@
 
 package com.google.sps;
 
+import com.google.common.collect.ImmutableList; 
+import com.google.common.collect.ImmutableSet; 
+import com.google.common.collect.Sets; 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set; 
+import java.util.stream.*; 
 
 /** A class to determine appropriate meeting times */
 public final class FindMeetingQuery {
 
-  /** Returns a list of time ranges such that a meeting with the specified constraints can be held
-  within each time range while still respecting the event calendar */
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+  private static final int LENGTH_OF_DAY = 24 * 60; 
+
+  /** 
+  * Returns a list of time ranges such that a meeting with the specified constraints can be held
+  * within each time range while still respecting the event calendar 
+  */
+  public static Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     
     long meetingDuration = request.getDuration(); 
     Collection<String> mandatoryMeetingAttendees = request.getAttendees();
     Collection<String> optionalMeetingAttendees = request.getOptionalAttendees();
 
     // Return no availability if the requested meeting's duration is longer than one day
-    if (meetingDuration > (24 * 60)) {
+    if (meetingDuration > (LENGTH_OF_DAY)) {
         return Arrays.asList();
     }
 
-    List<TimeRange> mandatoryRanges = determineAvailability(events, mandatoryMeetingAttendees, meetingDuration);
-    List<TimeRange> optionalRanges = determineAvailability(events, optionalMeetingAttendees, meetingDuration);
+    ImmutableList<TimeRange> mandatoryRanges = determineAvailability(events, mandatoryMeetingAttendees, meetingDuration);
+    ImmutableList<TimeRange> optionalRanges = determineAvailability(events, optionalMeetingAttendees, meetingDuration);
     
-    // Return availability based upon the quantity of mandatory verus optional attendees 
+    // Determine if availability is dependent upon solely mandatory verus optional attendees 
     if (mandatoryMeetingAttendees.isEmpty() && optionalMeetingAttendees.isEmpty()) {
         // Return availability as the entire day if there are no mandatory nor optional attendees 
         return Arrays.asList(TimeRange.WHOLE_DAY);
-    } else if (optionalMeetingAttendees.isEmpty()) {
-        // Return availability of the mandatory attendees if there are no optional attendees 
+    } if (!mandatoryMeetingAttendees.isEmpty() && optionalMeetingAttendees.isEmpty()) {
         return mandatoryRanges; 
-    } else if (mandatoryMeetingAttendees.isEmpty()) {
-        // Return availability of the optional attendees if there are no mandatory attendees 
+    } if (mandatoryMeetingAttendees.isEmpty() && !optionalMeetingAttendees.isEmpty()) {
         return optionalRanges; 
-    } else {
-        // Merge mandatory and optional attendees 
-        List<String> mergedAttendees = new ArrayList<>(mandatoryMeetingAttendees);
-        mergedAttendees.addAll(optionalMeetingAttendees);
+    } 
+        
+    // Merge mandatory and optional attendees 
+    List<String> mergedAttendees = new ArrayList<>(mandatoryMeetingAttendees);
+    mergedAttendees.addAll(optionalMeetingAttendees);
 
-        // Return availability of all attendees if possible, otherwise return availability of mandatory attendees
-        List<TimeRange> mergedRanges = determineAvailability(events, mergedAttendees, meetingDuration);
-        return mergedRanges.isEmpty() ? mandatoryRanges : mergedRanges; 
-    }
+    // Return availability of all attendees if possible, otherwise return availability of mandatory attendees
+    ImmutableList<TimeRange> mergedRanges = determineAvailability(events, mergedAttendees, meetingDuration);
+    return mergedRanges.isEmpty() ? mandatoryRanges : mergedRanges; 
   }
 
   // Determine the time ranges for which the meeting attendees can all mutually meet
-  private List<TimeRange> determineAvailability(Collection<Event> events, 
+  private static ImmutableList<TimeRange> determineAvailability(Collection<Event> events, 
     Collection<String> meetingAttendees, long meetingDuration) {
 
-    List<TimeRange> busyTimes = new ArrayList<>();
-
-    // Acquire the time range of every event that shares an attendee with the meeting 
-    for (Event event: events) {
-      Collection<String> eventAttendees = event.getAttendees();
-      if (hasMutualAttendees(eventAttendees, meetingAttendees)) {
-        busyTimes.add(event.getWhen()); 
-      }
-    }
-
-    // Sort these 'busy times' in order of ascending start time 
-    Collections.sort(busyTimes, TimeRange.ORDER_BY_START);
+    // Obtain a list of busy time ranges such that a meeting cannot be held during these ranges
+    List<TimeRange> busyTimes = events.stream() 
+    // Remove events that do not have mutual attendees with the meeting
+    .filter(event -> hasMutualAttendees(event.getAttendees(), meetingAttendees))
+    // Obtain the time range of each relevant event
+    .map(Event::getWhen)
+    // Sort the time ranges in order of increasing start time
+    .sorted(TimeRange.ORDER_BY_START)
+    .collect(Collectors.toList());
 
     // Obtain the inverse of the 'busy times' (i.e. aquire the 'free times')
-    List<TimeRange> freeTimes = inversify(busyTimes, meetingDuration);
+    ImmutableList<TimeRange> freeTimes = inversify(busyTimes, meetingDuration);
 
     // Sort and return the 'free times' in order of ascending start time 
-    Collections.sort(freeTimes, TimeRange.ORDER_BY_START);
-    return freeTimes;
+    ImmutableList<TimeRange> freeTimesSorted = ImmutableList.sortedCopyOf(TimeRange.ORDER_BY_START, freeTimes);
+    return freeTimesSorted; 
   }
 
   // Return true if an event and a meeting share an attendee, false otherwise
-  private boolean hasMutualAttendees(Collection<String> eventAttendees, Collection<String> meetingAttendees) {
-    for (String eventAttendee: eventAttendees) {
-       if (meetingAttendees.contains(eventAttendee)) {
-           return true; 
-       }
-    }
-    return false;
+  private static boolean hasMutualAttendees(Collection<String> eventAttendees, Collection<String> meetingAttendees) {
+    ImmutableSet<String> eventSet = ImmutableSet.copyOf(eventAttendees);
+    ImmutableSet<String> meetingSet = ImmutableSet.copyOf(meetingAttendees);
+    
+    return !Sets.intersection(eventSet, meetingSet).isEmpty(); 
   } 
 
   // Return the inverse of a schedule such that each returned range is an acceptable time to host a meeting
-  private List<TimeRange> inversify(Collection<TimeRange> busyTimes, long duration) {
+  private static ImmutableList<TimeRange> inversify(Collection<TimeRange> busyTimes, long duration) {
     List<TimeRange> freeTimes = new ArrayList();
     int startRange = TimeRange.START_OF_DAY; 
     int prevEnd = TimeRange.START_OF_DAY; 
@@ -121,6 +123,6 @@ public final class FindMeetingQuery {
       freeTimes.add(TimeRange.fromStartEnd(startRange, TimeRange.END_OF_DAY, true));
     }
 
-    return freeTimes; 
+    return ImmutableList.copyOf(freeTimes);
   }
 }
